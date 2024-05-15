6e94a1c0dfc1567c962a6e6596f8e9ccc74ee08b
Devashree Gosavi <35646522+devashreegosavi@users.noreply.github.com>
2024-05-15 16:37:43 +0530

    15 may 2024

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,12 +8,21 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit{
     loginform!: FormGroup;
+    organization : any = ['org1', 'org2', 'org3', 'org4'];
 
     ngOnInit(){
       this.loginform = new FormGroup({
         'userData' : new FormGroup({
+          'orgname' : new FormControl(null,[Validators.required]),
           'username' : new FormControl(null,[Validators.required]),
-          'email' : new FormControl(null,[Validators.required,Validators.email])
+          'password' : new FormControl(null,[Validators.required])
         })
+      });
+      
+    }
+
+    onSubmit(){
+      console.log(this.loginform.value);
+      this.loginform.reset();
     }
 }
